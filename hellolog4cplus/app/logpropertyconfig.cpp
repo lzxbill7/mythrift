8463d3f0b5721d6975c5a81920da2ebdf175b273
Leon liu <lzx_bupt@126.com>
2015-09-14 03:08:06 -0400

    [HELLO] Test log4cplus propery configuration files

---
+++ hellolog4cplus/app/logpropertyconfig.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file       logpropertyconfig.cpp
+ * @brief      /mythrift/hellolog4cplus/app/logpropertyconfig.cpp
+ *
+ * @date       Sep 14, 2015
+ * @author     lzx_bupt@126.com
+ * @note       create
+ */
+
+#include <log4cplus/configurator.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/helpers/stringhelper.h>
+#include <log4cplus/loggingmacros.h>
+
+using namespace std;
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+int main()
+{
+    tcout << LOG4CPLUS_TEXT("Entering main()...") << endl;
+    log4cplus::initialize();
+    LogLog::getLogLog()->setInternalDebugging(true);
+    Logger root = Logger::getRoot();
+    try
+    {
+        PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("../../hellolog4cplus/data/log4cplus.properties.in"));
+        Logger fileCat = Logger::getInstance(LOG4CPLUS_TEXT("filelogger"));
+        LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Testing...."));
+        LOG4CPLUS_WARN(root, LOG4CPLUS_TEXT("Writing messages to log...."));
+        for (int i = 0; i < 10000; ++i)
+            LOG4CPLUS_WARN(fileCat, LOG4CPLUS_TEXT("This is a WARNING...") << i);
+// Test that DOS EOLs in property files get removed.
+#define TEST_TEXT LOG4CPLUS_TEXT ("this is a test with DOS EOL-->")
+        tistringstream propsStream(
+        LOG4CPLUS_TEXT ("text=") TEST_TEXT LOG4CPLUS_TEXT ("\r\n"));
+        Properties props(propsStream);
+
+        // TODO： no such macro?
+        // LOG4CPLUS_ASSERT(root, props.getProperty(LOG4CPLUS_TEXT("text")) == TEST_TEXT);
+        assert(props.getProperty(LOG4CPLUS_TEXT("text")) == TEST_TEXT);
+    } catch (...)
+    {
+        tcout << LOG4CPLUS_TEXT("Exception...") << endl;
+        LOG4CPLUS_FATAL(root, LOG4CPLUS_TEXT("Exception occured..."));
+    }
+    tcout << LOG4CPLUS_TEXT("Exiting main()...") << endl;
+    return 0;
+}
+
