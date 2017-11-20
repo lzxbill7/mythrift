dac404b2f91949fe0119139f576335def914372c
Leon <leon@mobvista.com>
2017-11-20 17:37:41 +0800

    [HELLO] update outdate sentence

--- hellolog4cplus/app/logpatternlayout.cpp
+++ hellolog4cplus/app/logpatternlayout.cpp
@@ -33,7 +33,7 @@ int main()
         append_1->setName(LOG4CPLUS_TEXT("First"));
         log4cplus::tstring pattern = LOG4CPLUS_TEXT("%d{%m/%d/%y %H:%M:%S,%Q} [%t] %-5p %c{2} %%%x%% - %m [%l]%n");
         // std::tstring pattern = LOG4CPLUS_TEXT("%d{%c} [%t] %-5p [%.15c{3}] %%%x%% - %m [%l]%n");
-        append_1->setLayout(std::auto_ptr<Layout>(new PatternLayout(pattern)));
+        append_1->setLayout(std::unique_ptr<Layout>(new PatternLayout(pattern)));
         Logger::getRoot().addAppender(append_1);
         Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("test.a.long_logger_name.c.logger"));
         LOG4CPLUS_DEBUG(logger, "This is the FIRST log message...");
